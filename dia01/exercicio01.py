fac8756b97ed83f51b3774e2622107f276d0743b
Pedro Paulo <0901dantaspedro@gmail.com>
2025-01-30 17:42:52 -0300

    add: Resolução exercício 01

--- dia01/exercicio01.py
+++ dia01/exercicio01.py
@@ -5,3 +5,12 @@
 
 # dados = [10, 20, 42, 9, 12, 35, 24, 10, 8, 14, 21]
 
+import pandas as pd
+
+dados = [10, 20, 42, 9, 12, 35, 24, 10, 8, 14, 21]
+
+df = pd.Series(dados)
+
+print(f"Média: {df.describe()["mean"]}")
+print(f"Desvio padrão: {df.describe()["std"]}")
+print(f"Máximo Valor: {df.describe()["max"]}")
