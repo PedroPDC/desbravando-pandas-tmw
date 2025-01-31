285bbcd5d8eba595b582801c5b45c5974828db6f
Pedro Paulo <0901dantaspedro@gmail.com>
2025-01-31 20:08:26 -0300

    fix: solução menos verbosa

--- dia01/exercicio01.py
+++ dia01/exercicio01.py
@@ -11,6 +11,6 @@ dados = [10, 20, 42, 9, 12, 35, 24, 10, 8, 14, 21]
 
 series_dados = pd.Series(dados)
 
-print(f"Média: {series_dados.describe()["mean"]}")
-print(f"Desvio padrão: {series_dados.describe()["std"]}")
-print(f"Máximo Valor: {series_dados.describe()["max"]}")
+print(f"Média: {series_dados.mean()}")
+print(f"Desvio padrão: {series_dados.std()}")
+print(f"Máximo Valor: {series_dados.max()}")
